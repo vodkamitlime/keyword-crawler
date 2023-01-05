96cf436e949b60b78b620f17f1b283c0e19ad148
vodkamitlime <tejavascript@gmail.com>
2023-01-05 14:00:00 +0900

    feat: Swagger Document setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ScraperModule } from './scraper/scraper.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle(`CatchUp-${process.env.ENVIRONMENT}`)
+    .setDescription('APIs for keyword CatchUp crawler module')
+    .setVersion(`v${process.env.npm_package_version}`)
+    .build();
+  const catchUpSwagger = SwaggerModule.createDocument(app, swaggerOptions, {
+    include: [ScraperModule],
+  });
+  SwaggerModule.setup('spec', app, catchUpSwagger);
+  await app.listen(3003);
 }
 bootstrap();
