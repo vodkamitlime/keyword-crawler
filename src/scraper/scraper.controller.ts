b685bbd0d0b47e6e60cfa3654ce3952bd8263acb
vodkamitlime <tejavascript@gmail.com>
2023-01-07 23:30:08 +0900

    feat: 네이버 뷰  크롤러 1차 구현
    feat: 네이버 뷰 DTO 추가
    
    chore: delete unnecesasry file

--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,25 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
-
+import { Request, Response } from 'express';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { NaverViewResDTO } from 'src/common/dto/scraper-naver.dto';
 @Controller('scraper')
 export class ScraperController {
   constructor(private scraperService: ScraperService) {}
 
-  @Get()
-  async crawlNaverView() {
-    await this.scraperService.crawlNaverView();
+  @Get('/naver/view')
+  @ApiTags('Scraper-naver')
+  @ApiOkResponse({
+    type: NaverViewResDTO,
+  })
+  @ApiOperation({
+    summary: '네이버 View 영역 크롤러 모듈',
+    description: '주어진 키워드로, 지난 1주일 간의 게시글 크롤링',
+  })
+  async crawlNaverView(
+    @Res() res: Response,
+    @Query('keyword') keyword: string,
+  ): Promise<void> {
+    const resCrawlNaverView = await this.scraperService.crawlNaverView(keyword);
+    res.status(HttpStatus.OK).json(resCrawlNaverView);
   }
 
-  @Get('cafe')
-  async crawlNaverCafe() {
-    await this.scraperService.crawlNaverCafeArticle();
-  }
+  // @Get('cafe')
+  // async crawlNaverCafe() {
+  //   await this.scraperService.crawlNaverCafeArticle();
+  // }
 
   // @Get('blog')
   // async crawlNaverBlog() {
-  //   const res = await this.scraperService.crawlNaverBlog(
-  //     'https://blog.naver.com/haeun_yah/222279700857',
+  //   await this.scraperService.crawlNaverBlog(
+  //     'https://blog.naver.com/ek60044/222971891429',
   //   );
-  //   console.log(res);
   // }
 }
